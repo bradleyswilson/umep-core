a6b5a8631a5dd4fb75f885fe3bfc88ad474d694c
biglimp <biglimp@users.noreply.github.com>
2025-03-21 08:44:50 +0100

    Update skyviewfactor_algorithm.py
    Response to issue #8

--- umep/skyviewfactor_algorithm.py
+++ umep/skyviewfactor_algorithm.py
@@ -92,7 +92,10 @@ def generate_svf(
             raise ValueError("Mismatching spatial transform for DSM and CDSM.")
 
     # CDSM 2
-    cdsm_2_rast = np.zeros([rows, cols])
+    #cdsm_2_rast = np.zeros([rows, cols])
+    
+    trunk_ratio = trunk_ratio / 100.0
+    cdsm_2_rast = cdsm_rast * trunk_ratio #issue8
     # compute
     ret = svf.svfForProcessing153(dsm_rast, cdsm_rast, cdsm_2_rast, dsm_scale, use_cdsm)
 
